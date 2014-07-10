9fa55d8ab54d13a6023c29a271754a6b6a35cb9d
Kevin Kwon <matoson@gmail.com>
2014-07-10 10:47:05 +0900

    Initial Commit

---
+++ KKUITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KKUITableView
+//
+//  Created by hdk on 2014. 7. 10..
+//  Copyright (c) 2014년 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KKUITableViewTests/KKUITableViewTests.m
@@ -0,0 +1,34 @@
+//
+//  KKUITableViewTests.m
+//  KKUITableViewTests
+//
+//  Created by hdk on 2014. 7. 10..
+//  Copyright (c) 2014년 Kevin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KKUITableViewTests : XCTestCase
+
+@end
+
+@implementation KKUITableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
